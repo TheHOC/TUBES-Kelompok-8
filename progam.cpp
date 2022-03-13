4131d2b6dd4124447380ccad3676bae52aa7881a
Asweh Debrej <aswehdebrej@gmail.com>
2022-03-13 21:57:35 +0700

    nyoba kelas

---
+++ progam.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+#include <stdlib.h>
+
+class Robot {       // buat summon robot apapun
+    public: 
+        int health;
+        int damage;
+};
+
+void newBunshin() {
+
+}
+
+int main() {
+    return 0;
+};
