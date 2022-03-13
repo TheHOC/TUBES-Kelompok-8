2fb5d20beb391ee43d819886f4d2160b90142d2e
Asweh Debrej <aswehdebrej@gmail.com>
2022-03-13 23:48:21 +0700

    malem 1

--- progam.cpp
+++ progam.cpp
@@ -2,16 +2,37 @@
 #include <random>
 #include <stdlib.h>
 
-class Robot {       // buat summon robot apapun
+class Bunshin {       // buat summon robot apapun
     public: 
         int health;
         int damage;
 };
 
-void newBunshin() {
+class Robot {
+    public:
+        int health = 300;
+        int damage = 10;
+        int armor = 2;
+        void giliran() {
 
+        }
+};
+
+void newBunshin() {
+    
+    
+}
+
+void inti() {
+    int bunshins = 0;
+    
+    Robot player;
+    while (player.health > 0) {
+        player.giliran();
+    }
 }
 
 int main() {
     return 0;
 };
+
