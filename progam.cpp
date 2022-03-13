7ff19650c1f171c8dbc559f109b1e786669728ab
Asweh Debrej <aswehdebrej@gmail.com>
2022-03-14 05:15:31 +0700

    kerjaan malem

--- progam.cpp
+++ progam.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <random>
 #include <stdlib.h>
+#include <cmath>
 
 class Bunshin {       // buat summon robot apapun
     public: 
         int health;
         int damage;
+        int posX;
+        int posY;
+        void giliran() {
+            if (sqrt(pow(player.posX - posX, 2) + pow(player.posY - posY))) {
+
+            }
+        }
+        Bunshin() {
+            health = rand() % 20 + 21;
+            damage = rand() % 5 + 1;
+            posX = rand() % mapSizeX;
+            posY = rand() % mapSizeY;
+        }
 };
 
 class Robot {
     public:
         int health = 300;
         int damage = 10;
-        int armor = 2;
+        int posX = 0;
+        int posY = 0;
         void giliran() {
 
         }
@@ -25,7 +40,7 @@ void newBunshin() {
 
 void inti() {
     int bunshins = 0;
-    
+
     Robot player;
     while (player.health > 0) {
         player.giliran();
@@ -33,6 +48,9 @@ void inti() {
 }
 
 int main() {
+    Bunshin a;
+    int mapSizeX = 10;
+    int mapSizeY = 10;
     return 0;
 };
 
